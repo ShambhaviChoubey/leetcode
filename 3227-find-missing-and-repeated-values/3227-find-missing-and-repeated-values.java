a332f1356b6d2f4eef8c6a1481d68c546ef39514
ShambhaviChoubey <162912831+ShambhaviChoubey@users.noreply.github.com>
2025-06-07 17:39:46 +0530

    Time: 5 ms (39.07%), Space: 45.7 MB (25.84%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,38 +1,28 @@
-
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int length = grid.length, // Dimension of the grid
-            n = length * length, // Total number of Natural numbers
-            sumOfNNaturalNumber = n * (n + 1) / 2, // Sum of all Natural numbers till n
-            repeated = 0, missing = 0, // Result
-            sum = 0; // Sum of all elements in the Grid
-
-        // Holds all visited number related information
-        boolean[] nums = new boolean[n + 1];
-
-        // Scanning each row
-        for (int i = 0; i < length; i++) {
-
-            // Scanning each element in the column
-            for (int j = 0; j < length; j++) {
-
-                // If a number is already visited, then we have found our repeating element
-                if (nums[grid[i][j]]) {
-                    repeated = grid[i][j]; continue;
+        
+        HashSet<Integer> set =new HashSet<>();
+        int n=grid.length;
+        int sq=grid.length*grid.length;
+        int cursum=0;
+        int ans[]=new int[2];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(set.contains(grid[i][j]) )
+                {
+                    ans[0]=grid[i][j];
                 }
-
-                // Marking a number as visited in the nums array
-                nums[grid[i][j]] = true;
-
-                // Calculating the sum of elements in the array except for the repeated element
-                sum += grid[i][j];
+                else
+                {
+                    set.add(grid[i][j]);
+                    cursum=cursum+grid[i][j];
+                }
             }
         }
-
-        // Missing element is calulcated as the difference between sum of all natural number till n and the sum of all unique elements in the grid
-        missing = sumOfNNaturalNumber - sum;
-
-        // Return response
-        return new int[] {repeated, missing};
+    int totalsum=sq*(sq+1)/2;
+    ans[1]=totalsum-cursum;
+    return ans;
     }
 }
