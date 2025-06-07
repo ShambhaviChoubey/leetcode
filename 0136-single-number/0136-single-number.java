4a81aeb69a74e85ffffd9d0b8e1fa0c52ad36406
ShambhaviChoubey <162912831+ShambhaviChoubey@users.noreply.github.com>
2025-06-07 19:09:21 +0530

    Time: 6 ms (32.13%), Space: 46 MB (70.83%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> check = new HashSet<>();
-        int c[] = new int[1];
-        for (int num : nums) {
-            if (check.contains(num) )
-            {
-                check.remove(num);
-            } else
-                check.add(num);
+        Arrays.sort(nums);
+        if(nums.length==1){
+            return nums[0];
         }
-        return check.iterator().next();
+        for(int i=0;i<nums.length-1;i+=2){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+        }
+       return nums[nums.length-1];
     }
+    
 }
