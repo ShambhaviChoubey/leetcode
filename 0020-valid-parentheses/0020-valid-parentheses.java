50e36ffc9331aba22a22dfdfb771c8ed9db1ae22
ShambhaviChoubey <162912831+ShambhaviChoubey@users.noreply.github.com>
2025-07-03 19:04:42 +0530

    Time: 3 ms (39.54%), Space: 41.7 MB (65.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> check=new Stack<>();
-        for(int i: s.toCharArray())
-    {
-        if(i=='(')
-        {
-         check.push(')');
-        }
-        else if(i=='{')
-        {
-            check.push('}');
-        }
-        else if(i=='[')
-        {
-            check.push(']');
-        }
-        else if(check.isEmpty() || check.pop() !=i )
-        return false;
+     Stack<Character> Stack=new Stack<>();
+     java.util.Map<Character,Character> close=new java.util.HashMap<>();
+      close.put(')','(');
+      close.put('}','{');
+      close.put(']','[');
 
-    }
-        return check.isEmpty();
-    }
+       for(char c: s.toCharArray())
+       {
+        if(close.containsKey(c))
+        {
+            if(!Stack.isEmpty() && Stack.peek()==close.get(c))
+            {
+                Stack.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+            else
+            {
+                Stack.push(c);
+            }
+        }
+        return Stack.isEmpty();
+       }   
+    
 }
